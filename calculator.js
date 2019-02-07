869b3de47074a86326bb0dfebb014ac92e35d4e5
DESKTOP-6PSTR3V\vanes <vanessa.kirch88@gmail.com>
2019-02-06 17:18:52 -0700

    Start to building Calculator

---
+++ calculator.js
@@ -0,0 +1,62 @@
+function getNumber(num){
+  var input_var = document.getElementById('input');
+  switch(num){
+    case 1:
+      input_var.value += '1';
+      break;
+    case 2:
+      input_var.value += '2';
+      break;
+    case 3:
+      input_var.value += '3';
+      break;
+    case 4:
+      input_var.value += '4';
+      break;
+    case 5:
+      input_var.value += '5';
+      break;
+    case 6:
+      input_var.value += '6';
+      break;
+    case 7:
+      input_var.value += '7';
+      break;
+    case 8:
+      input_var.value += '8';
+      break;
+    case 9:
+      input_var.value += '9';
+      break;
+    case 0:
+      input_var.value += '0';
+      break;
+  }
+}
+
+//get the mathematical operation sign
+function getOperand(operand){
+  var input_var = document.getElementById('input');
+  switch(operand){
+    case '+':
+      input_var.value += '+';
+      break;
+    case '-';
+      input_var.value += '-';
+      break;
+    case 'x':
+      input_var.value += '*';
+      break;
+    case '/':
+      input_var.value += '/';
+      break;
+    case '+/-':
+      input_var.value += '-' + input_var.value;
+  }
+}
+
+//clear the screen
+function clearScreen(){
+  document.getElementById('input').value = "";
+  document.getElementById('answer').value = "";
+}
